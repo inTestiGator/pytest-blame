61ff7dbdd3b6e47454d176d00ee2b28ee8ee491e
Spencer Huang <huangs@allegheny.edu>
2019-04-02 16:49:38 -0400

    Reformat some files with black

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ from setuptools import setup, find_packages
 
 def read(filename):
     filepath = os.path.join(os.path.dirname(__file__), filename)
-    with io.open(filepath, mode='r', encoding='utf-8') as f:
+    with io.open(filepath, mode="r", encoding="utf-8") as f:
         return f.read()
 
 
 setup(
-    name='pytest-blame',
-    version='1.0.0',
-    description='A pytest plugin helps developers to debug by providing useful commits history.',
-    long_description=read('README.md'),
-    author='Lancaster Wu, Spencer Huang, Carson Quigley, Patrick Palad, Paul Livingston',
-    author_email='wuj@allegheny.edu, huangs@allegheny.edu, quigleyc@allegheny.edu, paladp@allegheny.edu, livingstonp@allegheny.edu',
-    url='https://github.com/inTestiGator/pytest-blame',
-    license='GNU',
-    platforms='any',
+    name="pytest-blame",
+    version="1.0.0",
+    description="A pytest plugin helps developers to debug by providing useful commits history.",
+    long_description=read("README.md"),
+    author="Lancaster Wu, Spencer Huang, Carson Quigley, Patrick Palad, Paul Livingston",
+    author_email="wuj@allegheny.edu, huangs@allegheny.edu, quigleyc@allegheny.edu, paladp@allegheny.edu, livingstonp@allegheny.edu",
+    url="https://github.com/inTestiGator/pytest-blame",
+    license="GNU",
+    platforms="any",
     packages=find_packages(),
-    install_requires=read('requirements.txt').splitlines(),
+    install_requires=read("requirements.txt").splitlines(),
 )
