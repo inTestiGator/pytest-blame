4caa7f3f75e97bf82194a3c417d37fdb136affd7
Spencer Huang <huangs@allegheny.edu>
2019-04-02 17:18:18 -0400

    Reformat code with black

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,17 +5,22 @@ from git import Repo
 
 def pytest_addoption(parser):
     """ Print stuff to header with --track """
-    group = parser.getgroup('track')
-    group.addoption("--track", action="store_true",
-                    help="pytest-blame help :D\n--track: show last git commit")
+    group = parser.getgroup("track")
+    group.addoption(
+        "--track",
+        action="store_true",
+        help="pytest-blame help :D\n--track: show last git commit",
+    )
 
 
 # pylint: disable=E1101
 def pytest_report_header():
     """ Display github commit in header """
-    if pytest.config.getoption('track'):
+    if pytest.config.getoption("track"):
         PATH = "."
         repo = Repo(PATH)
         commits = list(repo.iter_commits())
-        msg = print("\nLast passing commit --> ", commits[0].author, ":", commits[0].message)
+        msg = print(
+            "\nLast passing commit --> ", commits[0].author, ":", commits[0].message
+        )
     return msg
