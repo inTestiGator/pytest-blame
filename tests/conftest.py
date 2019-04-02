3c324c5abc97c18f84dbac78873627d1399f35e4
patrick <paladp@allegheny.edu>
2019-04-02 16:29:03 -0400

    Moves initial plugin code to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,18 @@
+import pytest
+from git import Repo
 
+def pytest_addoption(parser):
+    """ Print stuff to header with --track :D """
+    group = parser.getgroup('track')
+    group.addoption("--track", action="store_true",
+                    help="pytest-blame help :D\n--track: show last git commit")
+
+
+def pytest_report_header():
+    """ Display github commit in header """
+    if pytest.config.getoption('track'):
+        PATH="."
+        repo = Repo(PATH)
+        commits = list(repo.iter_commits())
+        msg = print("\nLast passing commit --> ", commits[0].author, ":", commits[0].message)
+        return msg
