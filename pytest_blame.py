970c77a59c52f753773a7e36f0f58b4e5347ed4c
quigley-c <quigleyc@allegheny.edu>
2019-04-02 15:39:17 -0400

    change blame.py to print most recent commit info

--- pytest_blame.py
+++ pytest_blame.py
@@ -1,13 +1,1 @@
-import pytest
 
-def pytest_addoption(parser):
-    """ Print stuff to header with --track :D """
-    group = parser.getgroup('track')
-    group.addoption("--track", action="store_true",
-                    help="pytest-blame help :D\n--track: show last git commit")
-
-
-def pytest_report_header():
-    """ Display github commit in header """
-    if pytest.config.getoption('track'):
-        return "User: username Commit: commit Status: FAILING"
