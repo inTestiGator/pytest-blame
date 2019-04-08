7fb2d26ba8573e3cebc739d2a435b41ac54bad72
Lancasterwu <wuj@allegheny.edu>
2019-04-08 15:13:26 -0400

    ignore pytest warning

--- tests/test_blame.py
+++ tests/test_blame.py
@@ -3,40 +3,43 @@ import pytest
 import pytest_blame
 
 
-def test_conftest(testdir):
+def test_conftest(testdir, capsys):
     """Make sure that our plugin works."""
     testdir.makeconftest(
         """
-        import pytest
-        from git import Repo
+import pytest
+from git import Repo
 
-        def pytest_addoption(parser):
-            group = parser.getgroup("track")
-            group.addoption(
-                "--track",
-                action="store_true",
-                help="pytest-blame help \n--track: show last git commit",
-            )
+def pytest_addoption(parser):
+    group = parser.getgroup('track')
+    group.addoption(
+        '--track',
+        action='store_true',
+        help='--track: show last git commit',
+        )
 
-        # pylint: disable=E1101
-        def pytest_report_header():
-            msg = print("Can't find the last passing commit")
-            if pytest.config.getoption("track"):
-                PATH = "."
-                repo = Repo(PATH)
-                commits = list(repo.iter_commits())
-                msg = print(
-                    "\nLast passing commit --> ", commits[0].author, ":", commits[0].message
-                )
-            return msg
-    """
+def pytest_report_header():
+    msg = print('Cannot find the last passing commit')
+    if pytest.config.getoption('track'):
+        PATH = '.'
+        repo = Repo(PATH)
+        commits = list(repo.iter_commits())
+        msg = print(
+            'Last passing commit --> ', commits[0].author, ':', commits[0].message
+        )
+    return msg
+"""
     )
     testdir.makepyfile(
         """
-        def test_add():
-            assert sample.add(1, 1) == 2
-            assert sample.add(0, 1) != 2
-    """
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(1, 1) == 2
+    assert add(0, 1) != 2
+"""
     )
-    result = testdir.runpytest()
-    result.assert_outcomes(passed=2)
+    testdir.runpytest()
+    standard_out, standard_err = capsys.readouterr()
+    assert "Cannot find the last passing commit" in standard_out
