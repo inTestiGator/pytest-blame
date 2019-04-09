f86eb0abe86409bbac23a2ccf473d3d883f5b178
Lancasterwu <wuj@allegheny.edu>
2019-04-08 21:56:37 -0400

    fixed linting error of getstatus

--- getstatus.py
+++ getstatus.py
@@ -1,9 +1,13 @@
-import requests
+"""get status of CI check from github"""
 import json
+import requests
 
 
 def getstatus(sha):
-    response = requests.get('https://api.github.com/repos/inTestiGator/pytest-blame/statuses/' + str(sha))
+    """get status of CI check from github"""
+    response = requests.get(
+        "https://api.github.com/repos/inTestiGator/pytest-blame/statuses/" + str(sha)
+    )
     statuses = json.loads(response.text)
     if statuses == []:
         check = "failure"
@@ -11,6 +15,3 @@ def getstatus(sha):
         state = statuses[0]
         check = state["state"]
     return check
-
-
-print(getstatus("063c3eab098a3054c468dfee08405e4ef055c261"))
