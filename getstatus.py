2d73f91f7c48c54b20c7050d2d8a9cd3c2574a32
Lancasterwu <wuj@allegheny.edu>
2019-04-08 22:51:48 -0400

    this commit shouldn't pass2

--- getstatus.py
+++ getstatus.py
@@ -5,7 +5,7 @@ import requests
 
 def getstatus(sha):
     response = requests.get(
-        "https://api.github.com/repos/inTestiGator/pytest-blame/statuses/" + str(sha)
+        "https://api.github.com/repos/inTestiGator/(sha)
     )
     statuses = json.loads(response.text)
     if statuses == []:
