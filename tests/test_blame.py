67956d189f3dea3ab262f0f695dbbb1cd70e2a1e
Lancaster Wu <wuj@allegheny.edu>
2019-04-15 23:57:33 -0400

    updating the test case

--- tests/test_blame.py
+++ tests/test_blame.py
@@ -1,19 +1,20 @@
 """ Tests the pytest_blame.py plugin for pytest """
 
 import pytest
+import pytest_blame
 
 
 @pytest.fixture()
 def sample_test(testdir):
+    """temporal test file"""
     testdir.makepyfile("""
         def test_pass():
             assert 1 == 1
-
-        def test_fail():
-            assert 1 == 2
     """)
     return testdir
 
 
-def test_addoption():
+def test_getstatus(sample_test):
     """Make sure that our plugin works."""
+    result = sample_test.runpytest(​"--track"​)
+    assert "Cannot find the last passing commit" in result.stdout
