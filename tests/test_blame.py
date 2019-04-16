2d4ac056b937abbac1e284952211528154a1741d
Lancasterwu <wuj@allegheny.edu>
2019-04-15 23:04:51 -0400

    create a fixture

--- tests/test_blame.py
+++ tests/test_blame.py
@@ -1,5 +1,19 @@
 """ Tests the pytest_blame.py plugin for pytest """
 
+import pytest
+
+
+@pytest.fixture()
+def sample_test(testdir):
+    testdir.makepyfile("""
+        def test_pass():
+            assert 1 == 1
+
+        def test_fail():
+            assert 1 == 2
+    """)
+    return testdir
+
 
 def test_addoption():
     """Make sure that our plugin works."""
