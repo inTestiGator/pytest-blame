9b12ff5857c7d8408254f5434bf232a6f11a90ea
Lancasterwu <wuj@allegheny.edu>
2019-04-16 14:18:51 -0400

    added new test function

--- tests/test_blame.py
+++ tests/test_blame.py
@@ -1,7 +1,6 @@
 """ Tests the pytest_blame.py plugin for pytest """
 
 import pytest
-import pytest_blame
 
 
 @pytest.fixture()
@@ -14,5 +13,10 @@ def sample_test(testdir):
     return testdir
 
 
-def test_getstatus(sample_test):
-    """Make sure that our plugin works."""
+def test_plugin(testdir):
+    testdir.copy_example("tests/test_sample.py")
+    testdir.runpytest("--track", "test_sample")
+
+
+def test_sample():
+    pass
